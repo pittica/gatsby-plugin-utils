92442e763e4f9b7a05af74bb9f904bf233f61573
Lucio Benini <iiriya@iiriya.com>
2021-09-01 13:48:08 +0200

    Fixies

--- index.js
+++
@@ -1,7 +0,0 @@
-const { joinLocale } = require("./src/joinLocale")
-const { formatLocale } = require("./src/formatLocale")
-const { commalify } = require("./src/commalify")
-const { groupify } = require("./src/groupify")
-const { categorify } = require("./src/categorify")
-
-module.exports = { joinLocale, formatLocale, commalify, groupify, categorify }
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const { joinLocale } = require("./functions/joinLocale")
+const { formatLocale } = require("./functions/formatLocale")
+const { commalify } = require("./functions/commalify")
+const { groupify } = require("./functions/groupify")
+const { categorify } = require("./functions/categorify")
+
+module.exports = { joinLocale, formatLocale, commalify, groupify, categorify }
